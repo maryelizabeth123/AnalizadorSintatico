2bbe25e77f8540931067c83b86d9a9bcb7867d80
MARY ELIZABETH PENA PAULINO <MARYPENA@ALUMNO.UTESA.EDU>
2021-02-13 21:59:13 -0400

    Analizador sintatico por Mary Elizabeth Pena Paulino

---
+++ AnalizadorSintatico/AnalizadorSintatico.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace AnalizadorSintatico
+{
+    partial class AnalizadorSintatico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodigo = new System.Windows.Forms.RichTextBox();
+            this.tblResultado = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tblResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(12, 58);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(292, 341);
+            this.txtCodigo.TabIndex = 0;
+            this.txtCodigo.Text = "";
+            // 
+            // tblResultado
+            // 
+            this.tblResultado.AllowUserToAddRows = false;
+            this.tblResultado.AllowUserToDeleteRows = false;
+            this.tblResultado.BackgroundColor = System.Drawing.Color.White;
+            this.tblResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblResultado.Location = new System.Drawing.Point(332, 58);
+            this.tblResultado.Name = "tblResultado";
+            this.tblResultado.RowHeadersVisible = false;
+            this.tblResultado.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.tblResultado.Size = new System.Drawing.Size(292, 341);
+            this.tblResultado.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Salmon;
+            this.label1.Location = new System.Drawing.Point(77, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(477, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ANALIZADOR SINTÁTICO POR MARY ELIZABETH PEÑA PAULINO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Salmon;
+            this.label2.Location = new System.Drawing.Point(111, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CÓDIGO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Salmon;
+            this.label3.Location = new System.Drawing.Point(452, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ANÁLISIS";
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnAnalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnalizar.ForeColor = System.Drawing.Color.Salmon;
+            this.btnAnalizar.Location = new System.Drawing.Point(391, 407);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(149, 31);
+            this.btnAnalizar.TabIndex = 2;
+            this.btnAnalizar.Text = "ANALIZAR";
+            this.btnAnalizar.UseVisualStyleBackColor = false;
+            this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.ForeColor = System.Drawing.Color.Salmon;
+            this.btnLimpiar.Location = new System.Drawing.Point(236, 407);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(149, 31);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.Color.Salmon;
+            this.btnSalir.Location = new System.Drawing.Point(81, 407);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(149, 31);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // AnalizadorSintatico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(640, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAnalizar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tblResultado);
+            this.Controls.Add(this.txtCodigo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AnalizadorSintatico";
+            this.ShowIcon = false;
+            this.Text = "Analizador Sintático";
+            this.Load += new System.EventHandler(this.AnalizadorSintatico_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tblResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtCodigo;
+        private System.Windows.Forms.DataGridView tblResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ AnalizadorSintatico/AnalizadorSintatico.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AnalizadorSintatico
+{
+    public partial class AnalizadorSintatico : Form
+    {
+        DataTable analisis = new DataTable();
+        List<char> delimitadores = new List<char>(new char[] { '(', ')', ':', ';', '[', ']', '{', '}' });
+        List<char> constante = new List<char>(new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+        List<char> identificadores = new List<char>(new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' });
+        List<char> operadores = new List<char>(new char[] { '+', '-', '*', '/' });
+
+        public AnalizadorSintatico()
+        {
+            InitializeComponent();
+        }
+
+        private void AnalizadorSintatico_Load(object sender, EventArgs e)
+        {
+            analisis.Columns.Add("Token", typeof(char));
+            analisis.Columns.Add("Lexema", typeof(string));
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtCodigo.Text = "";
+            analisis.Clear();
+            tblResultado.DataSource = null;
+            tblResultado.Refresh(); 
+           
+        }
+
+        private void btnAnalizar_Click(object sender, EventArgs e)
+        {
+            analisis.Clear();
+
+            List<char> _elementos = txtCodigo.Text.Replace(" ", "").ToCharArray().ToList();
+
+            if (_elementos.Count > 0)
+            {
+                DataRow fila;
+
+                foreach (char elemento in _elementos)
+                {
+                    fila = analisis.NewRow();
+
+                    if (constante.Contains(elemento))
+                    {
+                        fila["Token"] = elemento;
+                        fila["Lexema"] = "CONSTANTE";
+                    }
+                    else if (delimitadores.Contains(elemento))
+                    {
+                        fila["Token"] = elemento;
+                        fila["Lexema"] = "DELIMITADOR";
+                    }
+                    else if (operadores.Contains(elemento))
+                    {
+                        fila["Token"] = elemento;
+                        fila["Lexema"] = "OPERADOR";
+                    }
+                    else if (identificadores.Contains(elemento.ToString().ToUpper()[0]))
+                    {
+                        fila["Token"] = elemento;
+                        fila["Lexema"] = "IDENTIFICADOR";
+                    }
+                    else
+                    {
+                        fila["Token"] = elemento;
+                        fila["Lexema"] = "ERROR";
+                    }
+
+                    analisis.Rows.Add(fila);
+                }
+
+                tblResultado.DataSource = analisis;
+                tblResultado.Refresh();
+            }
+            else
+            {
+                tblResultado.DataSource = null;
+                tblResultado.Refresh();
+            }
+        }
+    }
+}
